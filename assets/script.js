b939690975ce4e926bb3480f71f6c72c871d409a
chaudeveloper <ms_michanguyen@yahoo.com>
2021-11-14 22:39:51 -0500

    adding visibility to slides to collapse and show whenever certain button was clicked but only start button work, not the first question

--- assets/script.js
+++ assets/script.js
@@ -3,20 +3,26 @@ var timerEl = document.getElementById('timer');
 function countdown() {
     var timeLeft = 75;
     var timeInterval = setInterval(function () {
-      if (timeLeft >= 1) {
+      if (timeLeft >= 0) {
         timerEl.textContent = timeLeft;
         timeLeft--;
       } else {
-        timerEl.textContent = '0';
         clearInterval(timeInterval);
-        window.alert="Timer is done";
+        document.getElementById("final").style.visibility = "visible";
       }
     }, 1000);
   }
 
   //will move this into startquiz function
- countdown();
 
- var questions = [
-     {question}
- ]
+
+ var start = document.querySelector("button")
+ start.onclick = function ()
+ {  countdown();
+    document.getElementById("blockquote").style.visibility = "collapse";
+    document.getElementById("one").style.visibility = "visible";
+ }
+var correctanswer = document.querySelector("1.3")
+correctanswer.addEventListener("click", function () {
+  window.alert = "you are correct"
+ })
